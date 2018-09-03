7ac8979dea1bcf5b0501e71ed493f8f53ba6fea8
David Tolnay <dtolnay@gmail.com>
2018-09-03 11:12:28 -0700

    Release 0.1.7

--- src/lib.rs
+++ src/lib.rs
@@ -138,7 +138,7 @@
 //! [#5]: https://github.com/dtolnay/mashup/issues/5
 //! [#6]: https://github.com/dtolnay/mashup/issues/6
 
-#![doc(html_root_url = "https://docs.rs/mashup/0.1.6")]
+#![doc(html_root_url = "https://docs.rs/mashup/0.1.7")]
 #![no_std]
 
 #[macro_use]
